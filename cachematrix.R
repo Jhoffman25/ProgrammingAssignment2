648217428c134129a9003a84ff52fcd150583a37
jhoff <Hoffman.jac@husky.neu.edu>
2018-08-09 22:05:02 -0500

    Adding the required changes to cachematrix
    Used the example provided for a template, then made the few changes for the program to work as it is intended. Made a few additional changes so that all of the variables made more sense with the program.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions, when combined, convert a matrix into the 'cacheMatrix' (the list) and then
+## solve for the inverse using the solve function. 
+## This is very similar to the example, because I used the example as a template.
 
-## Write a short comment describing this function
-
+## Takes in a matrix and converts it to the cache matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  
+  get <- function() x
+  setInverse <- function(Inverse) i <<- Inverse
+  getInverse <- function() i
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
-
+## Takes in the cache matrix object and solves for the inverse of the original matrix. Returns
+## the inverted matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  
+  data <- x$get()
+  ##Uses the solve function to get the inverse of the matrix
+  i <- solve(data, ...)
+  x$setInverse(i)
+  i
+  ## Returns the matrix that is the inverse of 'x' (the cached original matrix)
 }
